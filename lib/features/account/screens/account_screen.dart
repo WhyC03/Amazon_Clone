bd23b137685e5bd99bfe2e723c5a31a04621a02e
Yash Chandra <yash24.chandra2003@gmail.com>
2024-11-04 10:24:49 +0530

    Accounts Screen Done, Starting with Home Screen

--- lib/features/account/screens/account_screen.dart
+++ lib/features/account/screens/account_screen.dart
@@ -1,5 +1,7 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/account/widgets/below_app_bar.dart';
+import 'package:amazon_clone/features/account/widgets/orders.dart';
+import 'package:amazon_clone/features/account/widgets/top_buttons.dart';
 import 'package:flutter/material.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -48,7 +50,9 @@ class AccountScreen extends StatelessWidget {
         children: [
           BelowAppBar(),
           SizedBox(height: 10),
-          
+          TopButtons(),
+          SizedBox(height: 20),
+          Orders(),
         ],
       ),
     );
